cce282a82bb637e0f0e37cbb4331916910101b57
Jonathan Liaw <jonathan.liaw@beamdog.com>
2020-12-08 12:24:11 -0700

    d01/p01 solution

--- solutions/Day01.ts
+++ solutions/Day01.ts
@@ -1,12 +1,28 @@
-import { fileURLToPath } from "url";
 import File from "../util/File";
 
 async function run(): Promise<void> {
-  const input = new File("D:/dev/aoc2020/input/01.txt");
+  const input = new File("01.txt");
   const contents = await input.contents();
 
-  console.log(contents.length);
-  console.log(contents);
+  const code = contents
+    .map((s) => parseInt(s))
+    .reduce((res, current, index, arr) => {
+      if (index + 1 < arr.length && res === -1) {
+        const search = arr
+          .slice(index + 1)
+          .map((v) => ({ sum: v + current, index, value: v }))
+          .filter(({ sum }) => sum === 2020)
+          .pop();
+
+        if (!!search) {
+          return search.value * current;
+        }
+      }
+
+      return res;
+    }, -1);
+
+  console.log("part1 result is", code);
 }
 
 run();
--- util/File.ts
+++ util/File.ts
@@ -2,9 +2,10 @@ import { readFile } from "fs/promises";
 
 export default class File {
   private _path: string;
+  static DEFAULT_INPUT_PATH: string = "D:/dev/aoc2020/input/";
 
-  constructor(path: string) {
-    this._path = path;
+  constructor(filename: string, path?: string) {
+    this._path = (path ? path : File.DEFAULT_INPUT_PATH).concat(filename);
   }
 
   public async contents(): Promise<string[]> {
