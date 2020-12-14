61ffad621cbc773ea3689a24d2bcd05e5249e436
Jonathan Liaw <jonathan.liaw@beamdog.com>
2020-12-14 15:54:40 -0700

    d2p2 - slight counting error, otherwise framework is working

--- solutions/Day02.ts
+++ solutions/Day02.ts
@@ -4,17 +4,43 @@ function countChar(char: string, str: string): number {
   return str.split("").filter((c) => c === char).length;
 }
 
+function checkP2(i: number, j: number, char: string, pass: string): boolean {
+  let count = 0;
+
+  if (pass[i] === char) {
+    count += 1;
+  }
+
+  if (pass[j] === char) {
+    count += 1;
+  }
+
+  return count === 1;
+}
+
 async function run(): Promise<void> {
   const contents = await new File("02.txt").contents();
 
-  // PART 1
-  const p1RegExp = new RegExp(/([0-9]*)-([0-9]*) ([a-z]): ([a-z]*)/, "i");
-  const p1 = contents
-    .map((s) => p1RegExp.exec(s))
-    .map((r) => ({ min: parseInt(r[1]), max: parseInt(r[2]), char: r[3], pass: r[4] }))
-    .filter((r) => countChar(r.char, r.pass) >= r.min && countChar(r.char, r.pass) <= r.max);
+  const regexp = new RegExp(/([0-9]*)-([0-9]*) ([a-z]): ([a-z]*)/, "i");
+  const parsed = contents
+    .map((s) => regexp.exec(s))
+    .map((r) => ({ min: parseInt(r[1]), max: parseInt(r[2]), char: r[3], pass: r[4] }));
 
-  console.log(p1.length);
+  // PART 1
+  const p1 = parsed.filter(({ pass, min, max, char }) => countChar(char, pass) >= min && countChar(char, pass) <= max);
+
+  console.log("part 1 count:", p1.length);
+
+  // PART 2
+  const p2 = parsed.reduce((count, { char, max, min, pass }) => {
+    if (checkP2(min, max, char, pass)) {
+      count += 1;
+    }
+
+    return count;
+  }, 0);
+
+  console.log("part 2 count:", p2);
 }
 
 run();
